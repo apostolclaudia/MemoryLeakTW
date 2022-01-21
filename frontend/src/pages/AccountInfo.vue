97067b3d8f020b138666d5e11f38c512f979ac17
nicole-ana-maria-alexandru <nicoleanamaria.alexandru@gmail.com>
2022-01-22 00:55:23 +0200

    accountinfo facut

--- frontend/src/pages/AccountInfo.vue
+++ frontend/src/pages/AccountInfo.vue
@@ -1,67 +1,92 @@
 <template>
-  <q-page>
-    <div class="row items-center">
-      <div class="col col-label">
-        Username:
+  <q-page class="column items-center">
+    <q-card
+      class="form-title-class text-white text-center"
+      round
+      flat
+      bordered
+      style="background: #6ab654"
+    >
+      <q-card-section>
+        <div class="text-h6">UPDATE ACCOUNT INFO</div>
+      </q-card-section>
+    </q-card>
+
+    <q-form @submit="onSubmit" class="q-gutter-md" style="margin-top: 30px">
+
+      <q-input
+        outlined
+        v-model="username"
+        label="Username *"
+        lazy-rules
+        :rules="[ val => val && val.length > 0 || 'Please type a name']"
+      />
+
+      <q-input
+        outlined
+        v-model="firstName"
+        label="First Name *"
+        lazy-rules
+        :rules="[
+           val => val && val.length > 0 || 'Please type a first name'
+        ]"
+      />
+
+      <q-input
+        outlined
+        v-model="lastName"
+        label="Last Name *"
+        lazy-rules
+        :rules="[
+           val => val && val.length > 0 || 'Please type a last name'
+        ]"
+      />
+
+      <div>
+        <q-btn label="Submit" type="submit" color="primary" />
       </div>
-      <div class="col">
-        <q-input outlined v-model="text" class="width-input"/>
-      </div>
-    </div>
-    <div class="row items-center">
-      <div class="col col-label">
-        Email:
-      </div>
-      <div class="col">
-        <q-input outlined v-model="text" class="width-input"/>
-      </div>
-    </div>
-    <div class="row items-center">
-      <div class="col col-label">
-        First Name:
-      </div>
-      <div class="col">
-        <q-input outlined v-model="text" class="width-input"/>
-      </div>
-    </div>
-    <div class="row items-center">
-      <div class="col col-label">
-        Last Name:
-      </div>
-      <div class="col">
-        <q-input outlined v-model="text" class="width-input"/>
-      </div>
-    </div>
+    </q-form>
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from "vue";
-import { ref } from 'vue'
+import { ref } from 'vue';
+import { useQuasar } from "quasar";
 
 export default defineComponent({
   name: "PageAccountInfo",
-  setup () {
+  setup() {
+    const $q = useQuasar();
 
-    const username = ref('username')
-    const cbAvailable = ref(false)
-    const date = ref(null)
-    const quantity = ref(null)
-    const category = ref(null)
+    const username = ref(null)
+    const firstName = ref(null)
+    const lastName = ref(null)
+
 
     return {
-      text: ref('@username'),
-      dense: ref(false)
+      username,
+      firstName,
+      lastName,
+      onSubmit () {
+          $q.notify({
+            color: 'green-4',
+            textColor: 'white',
+            icon: 'cloud_done',
+            message: 'Submitted'
+          })
+      }
     }
   }
 });
 </script>
 
 <style lang="scss" scoped>
-.col-label{
-    text-align: right;
-}
-.width-input{
-    width: 5em;
+.form-title-class {
+  margin-top: 40px;
+  width: 100%;
+  max-width: 400px;
+  border-color: $secondary;
+  border-width: 2.5px;
 }
 </style>
